f0833a830a51e4a572bfd4173af34fcb45d462b0
Rahul Kaddi <iamrkaddi@gmail.com>
2025-05-13 09:01:55 +0530

    adding miscellenous files

---
+++ Miscellenous/Backend/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.use(express.urlencoded({extende:true}));
+app.use(express.json());
+
+app.get("/register", (req,res)=>{
+    res.send("STD GET req");
+});
+
+app.post("/register", (req,res)=>{
+    let {user,password} = req.body; 
+    console.log(req.body);
+    res.send(`STD POST req , Welcome ${user}`);
+});
+
+app.listen(port ,()=>{
+    console.log(`server is running on port ${port}`);
+});
