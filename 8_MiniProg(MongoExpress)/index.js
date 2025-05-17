bb321931984480586f0e35297e64261601e06a1c
Rahul Kaddi <iamrkaddi@gmail.com>
2025-05-17 21:05:33 +0530

    Adding more routes

--- 8_MiniProg(MongoExpress)/index.js
+++ 8_MiniProg(MongoExpress)/index.js
@@ -8,6 +8,7 @@ const Chat = require("./models/chat.js");
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended:true})); //For getting forms data
 
 // Mongoose Setup
 main()
@@ -18,7 +19,7 @@ async function main() {
 }
 
 app.get("/",(req,res)=>{
-    res.send("Home Page");
+    res.render("home.ejs");
 })
 
 // Show all Chats
@@ -26,6 +27,31 @@ app.get("/chats" , async (req,res)=>{
     let chats = await Chat.find();
     console.log(chats);
     res.render("index.ejs" ,{chats});
+})
+
+// ADD NEW CHAT
+app.get("/chats/new", (req,res)=>{
+    res.render("new.ejs");
+})
+app.post("/chats" , (req,res)=>{
+    let {from,msg,to} = req.body;
+    let newChat = new Chat({
+        from:from,
+        to:to,
+        message:msg,
+        created_at:new Date()
+    });
+    newChat.save()
+    .then((data)=>console.log(data))
+    .catch((err)=>console.log(err));
+    res.redirect("/chats");
+});
+
+// EDIT Chat
+app.get("/chats/:id/edit" ,async (req,res)=>{
+    let {id} = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs" , {chat});
 })
 
 app.listen(port, ()=>{
